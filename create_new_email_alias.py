5912c7976b896cbf35d5c87dc126b9566584f056
Twilight Sparkle <19155609+Twi1ightSparkle@users.noreply.github.com>
2020-09-27 03:09:15 +0100

    Use random sub domain and no longer have url in address

--- create_new_email_alias.py
+++ create_new_email_alias.py
@@ -1,24 +1,26 @@
 import pyperclip
 import random
-import sys
-from subprocess import call
-from sys import argv
 
-
-if len(argv) == 2:
-	email = argv[1]
-elif len(argv) > 2:
-	print("Error. Only zero or one parameters accepted.")
-	sys.exit(1)
-else:
-	email = input("Enter the site name: ")
+# import sys
+# from sys import argv
+# if len(argv) == 2:
+# 	email = argv[1]
+# elif len(argv) > 2:
+# 	print("Error. Only zero or one parameters accepted.")
+# 	sys.exit(1)
+# else:
+# 	email = input("Enter the site name: ")
 
 characters = "abcdefghijklmnopqrstuvwxyz0123456789"
-email += "-"
-for i in range(5):
+email = ""
+for i in range(20):
 	r = random.SystemRandom()
 	email += r.choice(characters)
-email += "@twilightsparkle.dev"
+email += "@"
+for i in range(20):
+	r = random.SystemRandom()
+	email += r.choice(characters)
+email += ".twily.me"
 
 pyperclip.copy(email)
 
