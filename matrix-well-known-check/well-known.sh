44fda4c828a747ce96c32bc4c315cfe97268f260
Twilight Sparkle <19155609+Twi1ightSparkle@users.noreply.github.com>
2024-10-31 14:58:01 +0000

    Check SRV if no server well-known

--- matrix-well-known-check/well-known.sh
+++ matrix-well-known-check/well-known.sh
@@ -47,6 +47,9 @@ else
 
     prettyHeader "$domain Registration flows"
     synapseDomain="$(echo "$federationTesterResult" | jq '.WellKnownResult."m.server"' --raw-output)"
+    if [[ -z "$synapseDomain" ]]; then
+        synapseDomain="$(echo "$federationTesterResult" | jq '.DNSResult.SRVRecords[0].Target' --raw-output)"
+    fi
     curl --data '{}' --header 'content-type: application/json' --request POST --silent "${curlParams[@]}" \
         "https://$synapseDomain/_matrix/client/v3/register" | jq .
 
