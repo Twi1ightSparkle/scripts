d7b4752bca3a9ff00b48d238e9b25de2d0b19509
Twilight Sparkle <19155609+Twi1ightSparkle@users.noreply.github.com>
2025-05-29 09:19:54 +0000

    Break lines at 80 characters

--- matrix-well-known-check/well-known.sh
+++ matrix-well-known-check/well-known.sh
@@ -33,7 +33,8 @@ if [ -z "$domain" ]; then
     cat <<EOT
 Usage: ./well-known.sh <homeserver.domain> [additional cURL options]
 
-cURL options --location (follow redirects) and --silent (no progress output) are always set.
+cURL options --location (follow redirects) and --silent (no progress output) 
+are always set.
 EOT
 else
     shift
@@ -45,9 +46,11 @@ else
         "https://federationtester.matrix.org/api/report?server_name=$domain")"
     echo "$federationTesterResult" | jq .
 
-    synapseDomain="$(echo "$federationTesterResult" | jq '.WellKnownResult."m.server"' --raw-output)"
+    synapseDomain="$(echo "$federationTesterResult" \
+        | jq '.WellKnownResult."m.server"' --raw-output)"
     if [[ -z "$synapseDomain" ]]; then
-        synapseDomain="$(echo "$federationTesterResult" | jq '.DNSResult.SRVRecords[0].Target' --raw-output)"
+        synapseDomain="$(echo "$federationTesterResult" \
+            | jq '.DNSResult.SRVRecords[0].Target' --raw-output)"
     fi
 
     prettyHeader "$domain Login flows"
@@ -55,7 +58,8 @@ else
         "https://$synapseDomain/_matrix/client/v3/login" | jq .
 
     prettyHeader "$domain Registration flows"
-    curl --data '{}' --header 'content-type: application/json' --request POST --silent "${curlParams[@]}" \
+    curl --data '{}' --header 'content-type: application/json' --request POST \
+        --silent "${curlParams[@]}" \
         "https://$synapseDomain/_matrix/client/v3/register" | jq .
 
     prettyHeader "$domain DNS records"
@@ -64,21 +68,27 @@ else
     files=(client server support)
     for file in "${files[@]}"; do
         prettyHeader "$matrixUrl/$file headers"
-        headers="$(curl --location --silent "${curlParams[@]}" --dump-header - --output /dev/null "$matrixUrl/$file")"
+        headers="$(curl --location --silent "${curlParams[@]}" --dump-header - \
+            --output /dev/null "$matrixUrl/$file")"
         echo "$headers"
 
         redirectURL="$(echo "$headers" | grep "location: ")"
         if [[ -n "$redirectURL" ]]; then
-            redirectDomain="$(echo "$redirectURL" | sed -E 's#location: http(s)?://##g' | sed -E 's#/.*##g')"
+            redirectDomain="$(echo "$redirectURL" | \
+                sed -E 's#location: http(s)?://##g' | sed -E 's#/.*##g')"
             prettyHeader "$redirectDomain DNS records"
             echo -e "$domain redirects to $redirectDomain\n"
             dig +noall +answer all "$redirectDomain"
         fi
 
         prettyHeader "$matrixUrl/$file content"
-        if ! curl --location --silent "${curlParams[@]}" "$matrixUrl/$file" | jq . 2>/dev/null; then
-            echo -e "$matrixUrl/$file content is not valid JSON. (Output below is trimmed to first 10 lines)\n\n"
-            curl --location --silent "${curlParams[@]}" "$matrixUrl/$file" | head -10
+        if ! curl --location --silent "${curlParams[@]}" "$matrixUrl/$file" \
+            | jq . 2>/dev/null;
+        then
+            echo -e "$matrixUrl/$file content is not valid JSON. \
+(Output below is trimmed to first 10 lines)\n\n"
+            curl --location --silent "${curlParams[@]}" "$matrixUrl/$file" \
+                | head -10
         fi
     done
 fi
